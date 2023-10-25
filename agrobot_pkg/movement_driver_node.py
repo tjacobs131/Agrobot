9ac0f22aef5720e700d402f9ec67861ab272f997
Tom Jacobs <tomjacobs2003@hotmail.com>
2023-10-25 12:28:07 +0200

    Setup webots simulation

---
+++ agrobot_pkg/movement_driver_node.py
@@ -0,0 +1,53 @@
+import rclpy
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+
+HALF_DISTANCE_BETWEEN_WHEELS = 0.045
+WHEEL_RADIUS = 0.025
+
+class MovementDriver(Node):
+    def init(self, webots_node, properties):
+        self.__robot = webots_node.robot
+
+        self.__left_motor = self.__robot.getDevice('left wheel motor')
+        self.__right_motor = self.__robot.getDevice('right wheel motor')
+
+        self.__left_motor.setPosition(float('inf'))
+        self.__left_motor.setVelocity(0)
+
+        self.__right_motor.setPosition(float('inf'))
+        self.__right_motor.setVelocity(0)
+
+        self.__target_twist = Twist()
+
+        rclpy.init(args=None)
+        self.__node = rclpy.create_node('my_robot_driver')
+        self.__node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
+
+    def __cmd_vel_callback(self, twist):
+        self.__target_twist = twist
+
+    def step(self):
+        rclpy.spin_once(self.__node, timeout_sec=0)
+
+        forward_speed = self.__target_twist.linear.x
+        angular_speed = self.__target_twist.angular.z
+
+        command_motor_left = (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+        command_motor_right = (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+
+        self.__left_motor.setVelocity(command_motor_left)
+        self.__right_motor.setVelocity(command_motor_right)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    movementDriver = MovementDriver()
+
+    rclpy.spin(movementDriver)
+
+    movementDriver.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'agrobot_pkg'
+
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/agrobot.urdf']))
+data_files.append((os.path.join('share', package_name), glob('launch/*.launch.py')))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tjacobs',
@@ -23,6 +28,7 @@ setup(
             'camera_data = agrobot_pkg.camera_data_node:main',
             'camera_processing = agrobot_pkg.camera_processing_node:main',
             'movement_controller = agrobot_pkg.movement_controller_node:main',
+            'movement_driver = agrobot_pkg.movement_driver_node:main',
         ],
     },
 )
